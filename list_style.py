b2d8d8a0d41ecb6dc724e62ea74d17528d115d58
cassis163 <cas.aangeenbrug@gmail.com>
2020-05-22 14:30:51 +0200

    Add styling methods in list_style.py

--- list_style.py
+++ list_style.py
@@ -1,2 +1,23 @@
-def even_row(row):
-    return True
+def color_rows(series):
+    return [
+        'background-color: #D3D3D3'
+        if index % 2 == 0 and index > 5 else ''
+        for index, _ in enumerate(series)
+    ]
+
+def color_head(series):
+    return [
+        'background-color: grey'
+        if index == 5 else ''
+        for index, _ in enumerate(series)
+    ]
+
+def color_head_text(series):
+    return [
+        'color: white'
+        if index == 5 else ''
+        for index, _ in enumerate(series)
+    ]
+
+def resize_columns(series):
+    return ['width: 500px' for value in series]
