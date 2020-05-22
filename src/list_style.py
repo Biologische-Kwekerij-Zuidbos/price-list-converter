570776ff85e99b989c42c4604d13ea0e37ee4bfe
cassis163 <cas.aangeenbrug@gmail.com>
2020-05-22 16:37:19 +0200

    Add /src folder that contains every Python script except main.py

---
+++ src/list_style.py
@@ -0,0 +1,26 @@
+def style_dataframe(df):
+    return df.style \
+        .apply(color_rows, axis=0) \
+        .apply(color_head, axis=0) \
+        .apply(color_head_text, axis=0)
+
+def color_rows(series):
+    return [
+        'background-color: #D3D3D3'
+        if index % 2 == 0 and index > 5 else ''
+        for index, _ in enumerate(series)
+    ]
+
+def color_head(series):
+    return [
+        'background-color: grey'
+        if index == 5 else ''
+        for index, _ in enumerate(series)
+    ]
+
+def color_head_text(series):
+    return [
+        'color: white'
+        if index == 5 else ''
+        for index, _ in enumerate(series)
+    ]
