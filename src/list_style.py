bc2733065816e47fdaf3f77ec135d3d41a98d285
cassis163 <cas.aangeenbrug@gmail.com>
2020-05-22 22:29:41 +0200

    Update list_style.py to not style the table head

--- src/list_style.py
+++ src/list_style.py
@@ -1,26 +1,10 @@
 def style_dataframe(df):
     return df.style \
-        .apply(color_rows, axis=0) \
-        .apply(color_head, axis=0) \
-        .apply(color_head_text, axis=0)
+        .apply(color_rows, axis=0)
 
 def color_rows(series):
     return [
         'background-color: #e6e6e6'
         if index % 2 == 0 and index > 5 else ''
-        for index, _ in enumerate(series)
-    ]
-
-def color_head(series):
-    return [
-        'background-color: grey'
-        if index == 5 else ''
-        for index, _ in enumerate(series)
-    ]
-
-def color_head_text(series):
-    return [
-        'color: white'
-        if index == 5 else ''
         for index, _ in enumerate(series)
     ]
