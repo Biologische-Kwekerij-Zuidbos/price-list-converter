ebe46786cb0d7075fc6b89e7b933c0db3ad2537a
cassis163 <cas.aangeenbrug@gmail.com>
2020-05-22 19:44:30 +0200

    Change colors in list_style.py

--- src/list_style.py
+++ src/list_style.py
@@ -1,26 +1,18 @@
 def style_dataframe(df):
     return df.style \
         .apply(color_rows, axis=0) \
-        .apply(color_head, axis=0) \
-        .apply(color_head_text, axis=0)
+        .apply(color_head, axis=0)
 
 def color_rows(series):
     return [
-        'background-color: #D3D3D3'
+        'background-color: #e6e6e6'
         if index % 2 == 0 and index > 5 else ''
         for index, _ in enumerate(series)
     ]
 
 def color_head(series):
     return [
-        'background-color: grey'
-        if index == 5 else ''
-        for index, _ in enumerate(series)
-    ]
-
-def color_head_text(series):
-    return [
-        'color: white'
+        'background-color: #f2f2f2'
         if index == 5 else ''
         for index, _ in enumerate(series)
     ]
