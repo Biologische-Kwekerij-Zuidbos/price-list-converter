17ff7b576f7375dc52459c5af4693067a25f8bb4
cassis163 <cas.aangeenbrug@gmail.com>
2020-06-15 21:55:11 +0200

    Change first letters in filetypes to be capitalized

--- src/input.py
+++ src/input.py
@@ -4,14 +4,11 @@ from tkinter import filedialog
 from tkinter import Tk
 
 def get_dirs():
-    root = Tk()
-    root.filenames = filedialog.askopenfilenames(
+    return filedialog.askopenfilenames(
         initialdir="C:/Downloads",
         title="Select file(s)",
-        filetypes=(("excel files","*.xls"), ("all files","*.*"))
+        filetypes=(("Excel files","*.xls"), ("All files","*.*"))
     )
-
-    return root.filenames
 
 def get_readings(dirs):
     return [pd.read_html(_dir) for _dir in dirs]
