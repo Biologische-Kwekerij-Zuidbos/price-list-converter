48c95cbc22628b8ff7b2f1aea63eb40333bc9005
cassis163 <cas.aangeenbrug@gmail.com>
2020-06-15 22:01:24 +0200

    CHange input.py to use tkinter the conventional way

--- src/input.py
+++ src/input.py
@@ -4,11 +4,14 @@ from tkinter import filedialog
 from tkinter import Tk
 
 def get_dirs():
-    return filedialog.askopenfilenames(
+    root = Tk()
+    root.filenames = filedialog.askopenfilenames(
         initialdir="C:/Downloads",
         title="Select file(s)",
         filetypes=(("Excel files","*.xls"), ("All files","*.*"))
     )
+
+    return root.filenames
 
 def get_readings(dirs):
     return [pd.read_html(_dir) for _dir in dirs]
